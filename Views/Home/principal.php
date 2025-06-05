7f886483ef52ccaffab70baa3fccc8f7029743d5
sebastianAcuna14 <sacuna40574@ufide.ac.cr>
2025-06-05 11:09:54 -0600

    Pantalla Principal

---
+++ Views/Home/principal.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>Add commentMore actions
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Proyecto MN</title>
+    <link href="../Estilos/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <div id="main-wrapper">
+       
+        <header class="topbar">
+            <nav class="navbar top-navbar navbar-expand-md navbar-dark">
+                <div class="navbar-header">
+                    <a class="nav-toggler waves-effect waves-light d-block d-md-none" href="javascript:void(0)">
+                        <i class="ti-menu ti-close"></i>
+                    </a>
+                    <div class="navbar-brand">
+                        <a href="index.html" class="logo">
+                            <b class="logo-icon">
+                                <img src="../Imagenes/logo-icon.png" alt="homepage" class="dark-logo" />
+                                <img src="../Imagenes/logo-light-icon.png" alt="homepage" class="light-logo" />
+                            </b>
+                            <span class="logo-text">
+                                <img src="../Imagenes/logo-text.png" alt="homepage" class="dark-logo" />
+                                <img src="../Imagenes/logo-light-text.png" class="light-logo" alt="homepage" />
+                            </span>
+                        </a>
+                        <a class="sidebartoggler d-none d-md-block" href="javascript:void(0)" data-sidebartype="mini-sidebar">
+                            <i class="mdi mdi-toggle-switch mdi-toggle-switch-off font-20"></i>
+                        </a>
+                    </div>
+                    <a class="topbartoggler d-block d-md-none waves-effect waves-light" href="javascript:void(0)" data-toggle="collapse" data-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <i class="ti-more"></i>
+                    </a>
+                </div>
+                <div class="navbar-collapse collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav float-left mr-auto">
+                        
+                    </ul>
+                    <ul class="navbar-nav float-right">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle waves-effect waves-dark pro-pic" href="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <img src="../Imagenes/2.jpg" alt="user" class="rounded-circle" width="40">
+                                <span class="m-l-5 font-medium d-none d-sm-inline-block">Jonathan Doe <i class="mdi mdi-chevron-down"></i></span>
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-right user-dd animated flipInY">
+                                <span class="with-arrow">
+                                    <span class="bg-primary"></span>
+                                </span>
+                                <div class="d-flex no-block align-items-center p-15 bg-primary text-white m-b-10">
+                                    <div class="">
+                                        <img src="../Imagenes/2.jpg" alt="user" class="rounded-circle" width="60">
+                                    </div>
+                                    <div class="m-l-10">
+                                        <h4 class="m-b-0">Jonathan Doe</h4>
+                                        <p class=" m-b-0">jon@gmail.com</p>
+                                    </div>
+                                </div>
+                                <div class="profile-dis scrollable">
+                                    <a class="dropdown-item" href="javascript:void(0)">
+                                        <i class="ti-user m-r-5 m-l-5"></i> My Profile</a>
+                                    <a class="dropdown-item" href="javascript:void(0)">
+                                        <i class="ti-wallet m-r-5 m-l-5"></i> My Balance</a>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+        <aside class="left-sidebar">
+            <div class="scroll-sidebar">
+                <nav class="sidebar-nav">
+                    <ul id="sidebarnav">
+                        <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="index.html" aria-expanded="false"><i class="ti-loop"></i><span class="hide-menu">Back To Home</span></a></li>
+                    </ul>
+                </nav>
+            </div>
+        </aside>
+        <div class="page-wrapper">
+            <div class="page-breadcrumb">
+                <div class="row">
+                    <div class="col-5 align-self-center">
+                        <h4 class="page-title">Starter Page</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+                       
+                    </div>
+                </div>
+            </div>
+            <footer class="footer text-center">
+                All Rights Reserved by Nice admin.
+            </footer>
+
+        </div>
+    </div>
+
+    <script src="../Funciones/jquery.min.js"></script>
+    <script src="../Funciones/popper.min.js"></script>
+    <script src="../Funciones/bootstrap.min.js"></script>
+    <script src="../Funciones/app.min.js"></script>
+    <script src="../Funciones/app.init.js"></script>
+    <script src="../Funciones/app-style-switcher.js"></script>
+    <script src="../Funciones/perfect-scrollbar.jquery.min.js"></script>
+    <script src="../Funciones/sparkline.js"></script>
+    <script src="../Funciones/waves.js"></script>
+    <script src="../Funciones/sidebarmenu.js"></script>
+    <script src="../Funciones/custom.min.js"></script>
+</body>
+
+</html>
