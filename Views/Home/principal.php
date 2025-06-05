811f581f3a35c9deefa8168054020f4129e10c3b
sebastianAcuna14 <sacuna40574@ufide.ac.cr>
2025-06-05 11:15:26 -0600

    Cambios visuales

--- Views/Home/principal.php
+++ Views/Home/principal.php
@@ -48,7 +48,6 @@
                     <ul class="navbar-nav float-right">
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle waves-effect waves-dark pro-pic" href="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <img src="../Imagenes/2.jpg" alt="user" class="rounded-circle" width="40">
                                 <span class="m-l-5 font-medium d-none d-sm-inline-block">Jonathan Doe <i class="mdi mdi-chevron-down"></i></span>
                             </a>
                             <div class="dropdown-menu dropdown-menu-right user-dd animated flipInY">
@@ -56,9 +55,6 @@
                                     <span class="bg-primary"></span>
                                 </span>
                                 <div class="d-flex no-block align-items-center p-15 bg-primary text-white m-b-10">
-                                    <div class="">
-                                        <img src="../Imagenes/2.jpg" alt="user" class="rounded-circle" width="60">
-                                    </div>
                                     <div class="m-l-10">
                                         <h4 class="m-b-0">Jonathan Doe</h4>
                                         <p class=" m-b-0">jon@gmail.com</p>
